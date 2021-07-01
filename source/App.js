72df0514f69d848d25e9ceabf5e71b4286374fd4
yovanha <yfajardo0@ucol.mx>
2021-07-01 11:17:56 -0500

    File location changed and sintax fixed

--- source/App.js
+++ source/App.js
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
+
 export default function App() {
   return (
-    <View style={styles.container}> 
+    <View style={styles.container}>
       <StatusBar style="auto" />
     </View>
   );
