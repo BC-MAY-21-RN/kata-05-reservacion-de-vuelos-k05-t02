bf82ddfbabb0f08ff9922361b601492bab4f1503
yovanha <yfajardo0@ucol.mx>
2021-07-01 09:27:33 -0500

    Checkbox added

--- source/App.js
+++ source/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import CheckBoxElements from './entities/signUp/atoms/CheckBoxElements';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <CheckBoxElements/>      
       <StatusBar style="auto" />
     </View>
   );
